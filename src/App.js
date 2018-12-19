dccde8802d479c35bb42673d48cbf333e7d10481
rogerwschmidt <rogerschmidt@gmail.com>
2018-12-19 15:46:28 -0800

    create baseline example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import shortId from 'shortid'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      todos: [
+        { id: shortId.generate(), description: 'Gotta go fast', completed: false },
+        { id: shortId.generate(), description: 'Recover chaos emeralds', completed: false },
+        { id: shortId.generate(), description: 'Find time to chill with Tails', completed: true },
+      ]
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>
+          <img src="https://yt3.ggpht.com/a-/AN66SAzMIYayieByccBHaOSxi-Tl5zluCmvk7sivKQ=s900-mo-c-c0xffffffff-rj-k-no" style={{width:'200px'}}/>
+        </div>
+        <ul>
+          {
+            this.state.todos.map(todo => 
+              <li 
+                style={todo.completed ? { textDecoration: 'line-through' } : null } >
+                {todo.description}
+              </li>
+            )
+          }
+        </ul>
       </div>
     );
   }
